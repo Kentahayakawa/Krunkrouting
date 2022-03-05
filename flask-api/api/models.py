a9a016fc7b99f4dfcbde52397716e1d18ffd25b3
KeiranGlynn <jamesglynn.k@gmail.com>
2022-03-05 04:55:39 -0800

    Minor Adjustment
    Adjusted the code so it uses one less database call

--- flask-api/api/models.py
+++ flask-api/api/models.py
@@ -130,25 +130,23 @@ class Votes(db.Model):
     group = db.relationship('Groups', backref=db.backref('votes', lazy=True), foreign_keys=[group_id])
 
     def save(self):
-        # Check for double voting; We only want one vote per user & bar.        
-        if not self.vote_exists(self.user_id, self.place_id):
+        # Check for double voting; We only want one vote per user & bar.
+        exists = self.find_vote(self.user_id, self.place_id)
+        if exists is None:
             db.session.add(self)
             db.session.commit()
     
     def delete(self):
-        # Make sure vote actually exists before you try to delete it.        
-        if self.vote_exists(self.user_id, self.place_id):
-            db.session.delete(db.session.query(Votes).filter(Votes.user_id==self.user_id, Votes.place_id==self.place_id).first())
+        # Make sure vote actually exists before you try to delete it.
+        exists = self.find_vote(self.user_id, self.place_id)
+        if exists is not None:
+            db.session.delete(exists)
             db.session.commit()
     
     @classmethod
-    def vote_exists(cls, user_id, place_id):
-        exists = db.session.query(Votes).filter(Votes.user_id==user_id, Votes.place_id==place_id).first()
-        if exists is not None:
-            return True
-        else:
-            return False
-
+    def find_vote(cls, user_id, place_id):
+        found_vote = db.session.query(Votes).filter(Votes.user_id==user_id, Votes.place_id==place_id).first()
+        return found_vote 
 
     def toJSON(self):
         return {'user_id': self.user_id, 'place_id': self.place_id}
