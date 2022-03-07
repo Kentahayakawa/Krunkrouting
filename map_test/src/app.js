6893d16b5605674a40b4cbd0526b04c092cc2b5f
KeiranGlynn <jamesglynn.k@gmail.com>
2022-03-07 08:30:44 -0800

    Removed Unused Code
    There was some code for adding custom markers and panning to them.
    
    Given our time constraints, I don't think we have time to do fancy stuff like that.

--- map_test/src/app.js
+++ map_test/src/app.js
@@ -12,8 +12,6 @@ const loader = new Loader(apiOptions);
 loader.load().then(() => {
     console.log('Maps JS API loaded');
     const map = displayMap();
-    //const markers = addMarkers(map); // adds pins for locations
-    //addPanToMarker(map, markers);
 });
 
 function displayMap() {
@@ -66,47 +64,3 @@ function calc_and_display_route(directionsService, directionsRenderer) {
         .catch((e) => window.alert("Directions request failed due to " + status));
 }
 
-/* Used to create marker objects that will be added to the map in the load 
-* callback function
-*/
-function addMarkers(map) {
-    const locations = {
-        operaHouse: { lat: -33.8567844, lng: 151.213108 },
-        tarongaZoo: { lat: -33.8472767, lng: 151.2188164 },
-        manlyBeach: { lat: -33.8209738, lng: 151.2563253 },
-        hyderPark: { lat: -33.8690081, lng: 151.2052393 },
-        theRocks: { lat: -33.8587568, lng: 151.2058246 },
-        circularQuay: { lat: -33.858761, lng: 151.2055688 },
-        harbourBridge: { lat: -33.852228, lng: 151.2038374 },
-        kingsCross: { lat: -33.8737375, lng: 151.222569 },
-        botanicGardens: { lat: -33.864167, lng: 151.216387 },
-        museumOfSydney: { lat: -33.8636005, lng: 151.2092542 },
-        maritimeMuseum: { lat: -33.869395, lng: 151.198648 },
-        kingStreetWharf: { lat: -33.8665445, lng: 151.1989808 },
-        aquarium: { lat: -33.869627, lng: 151.202146 },
-        darlingHarbour: { lat: -33.87488, lng: 151.1987113 },
-        barangaroo: { lat: - 33.8605523, lng: 151.1972205 }
-    }
-    const markers = [];
-    for (const location in locations) {
-        const markerOptions = {
-            map: map,
-            position: locations[location],
-            icon: './img/custom_pin.png'
-        }
-        const marker = new google.maps.Marker(markerOptions);
-        markers.push(marker);
-    }
-    return markers;
-}
-
-// This function re-centers the map over the provided 
-function addPanToMarker(map, markers) {
-    markers.map(marker => {
-        marker.addListener('click', event => {
-            const location = { lat: event.latLng.lat(), lng: event.latLng.lng() };
-            map.panTo(location);
-        });
-    });
-    return markers;
-}
