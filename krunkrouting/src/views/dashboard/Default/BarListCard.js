88dc1707f1555bec5e1e86ff36c9f007ae4516bc
Prakhar Sharma <prakhar6sharma@gmail.com>
2022-03-07 07:02:37 -0800

    Added support to show the current bars in the UI

--- krunkrouting/src/views/dashboard/Default/BarListCard.js
+++ krunkrouting/src/views/dashboard/Default/BarListCard.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 
 // material-ui
@@ -8,24 +8,25 @@ import { makeStyles } from '@material-ui/styles';
 import { Avatar, Button, Box, ButtonBase, CardActions, CardContent, Divider, Grid, Menu, MenuItem, Typography, Rating } from '@material-ui/core';
 
 // project imports
-import SubCard from './../../../ui-component/cards/SubCard';
+import SubCard from '../../../ui-component/cards/SubCard';
 import BajajAreaChartCard from './BajajAreaChartCard';
-import MainCard from './../../../ui-component/cards/MainCard';
-import configData from './../../../config';
-import SkeletonPopularCard from './../../../ui-component/cards/Skeleton/PopularCard';
-import { gridSpacing } from './../../../store/constant';
-import {CLEAR_SEARCH_RESULTS} from './../../../store/actions'; // SEARCH_RTL
+import MainCard from '../../../ui-component/cards/MainCard';
+import configData from '../../../config';
+import SkeletonPopularCard from '../../../ui-component/cards/Skeleton/PopularCard';
+import { gridSpacing } from '../../../store/constant';
+import {CLEAR_SEARCH_RESULTS} from '../../../store/actions'; // SEARCH_RTL
 
 // assets
 import axios from 'axios';
 import ChevronRightOutlinedIcon from '@material-ui/icons/ChevronRightOutlined';
 import MoreHorizOutlinedIcon from '@material-ui/icons/MoreHorizOutlined';
-import DeleteIcon from '@material-ui/icons/Delete';
+import RefreshIcon from '@material-ui/icons/Refresh';
+import Close from '@material-ui/icons/Close';
 import KeyboardArrowUpOutlinedIcon from '@material-ui/icons/KeyboardArrowUpOutlined';
 import KeyboardArrowDownOutlinedIcon from '@material-ui/icons/KeyboardArrowDownOutlined';
 
 // assets
-import { IconCirclePlus, IconTrash} from '@tabler/icons';
+import { IconCirclePlus, IconCircleMinus, IconTrash} from '@tabler/icons';
 
 // style constant
 const useStyles = makeStyles((theme) => ({
@@ -68,27 +69,36 @@ const useStyles = makeStyles((theme) => ({
 
 
 
-const IndividualSearchElement = ({ isLoading, bar_id, bar_name, bar_address, bar_rating, n_reviews, price_filter}) => {
+const IndividualBarElement = ({ isLoading, bar_id, bar_name, bar_address, bar_rating, n_reviews, price_filter, n_votes}) => {
 
     const classes = useStyles();
     let str = "$";
     const account = useSelector((state) => state.account);
-    const handleVote = () => {
+    const handlePositiveVote = () => {
         console.log(bar_id);
         axios
             .post(configData.API_SERVER + 'vote', {"place_id": bar_id, "choice": "True"}, {headers: {Authorization: `${account.token}`}} 
             )
             .then(function (response) {
                 console.log(response.data);
-                
-                
+            }).catch(function (error) {
+            });
+    }
+
+    const handleNegativeVote = () => {
+        console.log(bar_id);
+        axios
+            .post(configData.API_SERVER + 'vote', {"place_id": bar_id, "choice": "False"}, {headers: {Authorization: `${account.token}`}} 
+            )
+            .then(function (response) {
+                console.log(response.data);
             }).catch(function (error) {
             });
     }
 
     return (
         <Grid container direction="row" spacing={3} alignItems="center" justifyContent="space-between">
-            <Grid item xs={8}>
+            <Grid item xs={12} lg={6}>
                 <Grid container direction="column">
                     {/* <Grid container direction="column" spacing={1} > */}
                         <Grid item>
@@ -99,12 +109,12 @@ const IndividualSearchElement = ({ isLoading, bar_id, bar_name, bar_address, bar
                     </Grid>
                     <Grid item>
                         <Grid container direction="row" justifyContent="flex-start" spacing={1}>
-                            <Grid item lg={1.5} xs={1.5} justifyContent="center">
+                            <Grid item lg={1} xs={1.5} justifyContent="center">
                                 <Typography variant="h5" color="inherit">
                                     {bar_rating}
                                 </Typography>
                             </Grid>
-                            <Grid item lg={5} xs={7} justifyContent="center">
+                            <Grid item lg={3} xs={5} justifyContent="center">
                                 <Typography variant="subtitle2" color="inherit">
                                     <Rating name="read-only" value={bar_rating} readOnly precision={0.25} size="small"/>
                                 </Typography>
@@ -139,14 +149,32 @@ const IndividualSearchElement = ({ isLoading, bar_id, bar_name, bar_address, bar
                     </Grid>
                 </Grid>
             </Grid>
-            <Grid item xs={4} justifyContent="flex-end" alignItems="right">
-                <Box display="flex" justifyContent="flex-end">
-                    <ButtonBase onClick={handleVote} sx={{ borderRadius: '12px' }}>
-                        <Avatar variant="rounded" className={classes.headerAvatar}>
-                            <IconCirclePlus stroke={1.5} size="1.3rem" />
-                        </Avatar>
-                    </ButtonBase>
-                </Box>
+            <Grid item xs={6} lg={6} justifyContent="flex-end" alignItems="right">
+                <Grid container direction="row" spacing={1}>
+                    <Grid item xs={4}>
+                        <Typography variant="subtitle1" color="inherit">
+                            {n_votes}
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={4} alignItems="center">
+                        <Box display="flex" justifyContent="flex-end" >
+                            <ButtonBase onClick={handlePositiveVote} sx={{ borderRadius: '12px' }}>
+                                <Avatar variant="rounded" className={classes.headerAvatar}>
+                                    <IconCirclePlus stroke={1.5} size="1.3rem" />
+                                </Avatar>
+                            </ButtonBase>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={4} alignItems="right">
+                        <Box display="flex" justifyContent="flex-end">
+                            <ButtonBase onClick={handleNegativeVote} sx={{ borderRadius: '12px' }}>
+                                <Avatar variant="rounded" className={classes.headerAvatar}>
+                                    <IconCircleMinus stroke={1.5} size="1.3rem" />
+                                </Avatar>
+                            </ButtonBase>
+                        </Box>
+                    </Grid>
+                </Grid>
             </Grid>
         </Grid>
     );
@@ -154,29 +182,54 @@ const IndividualSearchElement = ({ isLoading, bar_id, bar_name, bar_address, bar
 };
 //-----------------------|| DASHBOARD DEFAULT - POPULAR CARD ||-----------------------//
 
-const SearchResultsCard = ({ isLoading }) => {
+const BarListCard = ({ isLoading }) => {
+    const handleBarList = () => {
+        axios
+            .post(configData.API_SERVER + 'users', {"user_id": account.user._id}, {headers: {Authorization: `${account.token}`}} 
+            )
+            .then(function (response) {
+                if (response.data) {
+                    axios
+                        .post(configData.API_SERVER + 'groups', {"group_id": response.data.group_id}, {headers: {Authorization: `${account.token}`}} 
+                        )
+                        .then(function (response2) {
+                            if (response2.data.success) {
+                                let tempbarlist = []
+                                for(const vote in response2.data.group.votes){
+                                    tempbarlist.push({
+                                        bar_id: response2.data.group.votes[vote]['place']['place_id'], 
+                                        bar_name: response2.data.group.votes[vote]['place']['name'],
+                                        bar_address: response2.data.group.votes[vote]['place']['address'],
+                                        bar_rating: response2.data.group.votes[vote]['place']['rating'],
+                                        n_reviews: response2.data.group.votes[vote]['place']['user_ratings_total'],
+                                        price_filter: response2.data.group.votes[vote]['place']['price_level'],
+                                        n_votes:response2.data.group.votes[vote]['num_votes']
+                                    })
+                                }
+                                tempbarlist.sort((a, b) => a.n_votes - b.n_votes)
+                                setBarList(tempbarlist);
+                            }
+                        }).catch(function (error) {
+                        });
+                }
+            }).catch(function (error) {
+            });
+        
+    }
+
+    const handleDeleteBarList = () => {
+        setBarList([]);
+    }
     
     const classes = useStyles();
     const account = useSelector((state) => state.account);
-    const dispatch = useDispatch();
+    const [barList, setBarList] = React.useState([]);
 
-    const [anchorEl, setAnchorEl] = React.useState(null);
-
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-        dispatch({type: CLEAR_SEARCH_RESULTS});
-    };
-
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-    const shouldRender = (account && account.search_results && account.search_results.length !==0)? !isLoading: 0 
-    console.log(shouldRender);
-    console.log("FETCHED search results")
-    console.log(account.search_results);
+    console.log("barList");
+    console.log(barList);
     return (
         <React.Fragment>
-            {!shouldRender ? (
+            {isLoading ? (
                 <SkeletonPopularCard />
             ) : (
                 <MainCard content={false}>
@@ -185,42 +238,15 @@ const SearchResultsCard = ({ isLoading }) => {
                             <Grid item xs={12}>
                                 <Grid container alignContent="center" justifyContent="space-between">
                                     <Grid item>
-                                        <Typography variant="h2">Search Results</Typography>
+                                        <Typography variant="h2">Bars your group has voted for: Ranked in priority</Typography>
                                     </Grid>
                                     <Grid item>
-                                    <DeleteIcon
+                                    <RefreshIcon
                                             className={classes.primaryLight}
                                             aria-controls="menu-popular-card"
                                             aria-haspopup="true"
-                                            onClick={handleClick}
+                                            onClick={handleBarList}
                                         />
-                                        {/* <MoreHorizOutlinedIcon
-                                            fontSize="small"
-                                            className={classes.primaryLight}
-                                            aria-controls="menu-popular-card"
-                                            aria-haspopup="true"
-                                            onClick={handleClick}
-                                        /> */}
-                                        {/* <Menu
-                                            id="menu-popular-card"
-                                            anchorEl={anchorEl}
-                                            keepMounted
-                                            open={Boolean(anchorEl)}
-                                            onClose={handleClose}
-                                            variant="selectedMenu"
-                                            anchorOrigin={{
-                                                vertical: 'bottom',
-                                                horizontal: 'right'
-                                            }}
-                                            transformOrigin={{
-                                                vertical: 'top',
-                                                horizontal: 'right'
-                                            }}
-                                        >
-                                            <MenuItem onClick={handleClose}> Today</MenuItem>
-                                            <MenuItem onClick={handleClose}> This Month</MenuItem>
-                                            <MenuItem onClick={handleClose}> This Year </MenuItem>
-                                        </Menu> */}
                                     </Grid>
                                 </Grid>
                             </Grid>
@@ -228,36 +254,31 @@ const SearchResultsCard = ({ isLoading }) => {
                                 <BajajAreaChartCard />
                             </Grid> */}
                             <Grid item xs={12}>
-                                {account.search_results.map((result) => (
+                                {barList.map((result) => (
                                     <SubCard>
-                                        <IndividualSearchElement
+                                        <IndividualBarElement
                                             key={result.place_id}
                                             bar_id={result.place_id}
-                                            bar_name={result.name}
-                                            bar_address={result.address}
-                                            bar_rating={result.rating}
-                                            n_reviews={result.user_ratings_total}
-                                            price_filter={result.price_level}
+                                            bar_name={result.bar_name}
+                                            bar_address={result.bar_address}
+                                            bar_rating={result.bar_rating}
+                                            n_reviews={result.n_reviews}
+                                            price_filter={result.price_filter}
+                                            n_votes={result.n_votes}
                                         />
                                     </SubCard>
                                 ))}
                             </Grid>
                         </Grid>
                     </CardContent>
-                    <CardActions className={classes.cardAction}>
-                        <Button size="small" disableElevation>
-                            View All
-                            <ChevronRightOutlinedIcon />
-                        </Button>
-                    </CardActions>
                 </MainCard>
             )}
         </React.Fragment>
     );
 };
 
-SearchResultsCard.propTypes = {
+BarListCard.propTypes = {
     isLoading: PropTypes.bool
 };
 
-export default SearchResultsCard;
+export default BarListCard;
