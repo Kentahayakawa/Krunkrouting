58cf4d54eda2e8a2b748c238ee9ffbe868f48502
Kenta Hayakawa <pentankentan@yahoo.com>
2022-03-07 10:10:02 -0800

    Fixed bugs

--- flask-api/tests.py
+++ flask-api/tests.py
@@ -19,31 +19,37 @@ def client(app):
 def new_user(app):
     user = Users(username="Bob", email="K@gmail.com", password="pass")
     return user
-    
+
+#testing database access
 def test_database(new_user):
     assert new_user.username == "Bob"
     assert new_user.email == "K@gmail.com"
     assert new_user.password == "pass"
 
+#testing valid input of user edit
 def test_user_edit(client):
     response = client.post('api/users/edit', json = {"email": "H@gmail.com"})
     assert response.status_code == 400
     assert not response.json.get("success")
 
+#testing valid input of Get/Post
 def test_bad_method(client):
     response = client.get('/api/vote')
     assert response.status_code == 405
 
+#testing json formatting
 def test_json_format(client):
     response = client.post('/api/vote', json='email')
     assert response.status_code == 400
     assert not response.json.get("success")
 
+#testing registration function
 def test_registration(client):
-    response = client.post('/api/users/register', data = json.dumps({"username": "Bon"}))
+    response = client.post('/api/users/register', data = json.dumps{"username": "Bon"})
     assert response.status_code == 400
     assert not response.json.get("success")
 
+#testing locations api
 def test_nearby(client):
     response = client.post('/api/places', json={"address": "1000 Gayley Avenue, Los Angeles, CA"})
     assert response.status_code == 405
